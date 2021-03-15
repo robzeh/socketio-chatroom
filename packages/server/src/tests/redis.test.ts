91c86b6afe62c5f12406655a0310d3e3738301b8
Robie Gonzales <robie.gonzaless@gmail.com>
2021-03-15 18:54:14 -0300

    test: :fire: removed done, should only be used for callbacks

--- packages/server/src/tests/redis.test.ts
+++ packages/server/src/tests/redis.test.ts
@@ -6,16 +6,15 @@ beforeAll((done) => {
 });
 
 // close redis connection
-afterAll((done) => {
+afterAll(() => {
   redis.disconnect();
-  done();
 });
 
 // session store
 describe('session store', () => {
 
   // save session
-  test('save session', async (done) => {
+  test('save session', async () => {
     sessionStore.saveSession('1234', {
       username: 'robie',
       roomId: 'room'
@@ -26,11 +25,10 @@ describe('session store', () => {
       username: 'robie',
       roomId: 'room'
     });
-    done();
   });
 
   // update session
-  test('update session', async (done) => {
+  test('update session', async () => {
     sessionStore.saveSession('1234', {
       username: 'new robie',
       roomId: 'new room'
@@ -41,17 +39,15 @@ describe('session store', () => {
       username: 'new robie',
       roomId: 'new room'
     });
-    done();
   });
 
   // find empty session
-  test('non existing session', async (done) => {
+  test('non existing session', async () => {
     const res = await sessionStore.findSession('0');
     expect(res).toEqual({
       username: null,
       roomId: null
     });
-    done();
   });
 
 });
@@ -60,44 +56,39 @@ describe('session store', () => {
 describe('room store', () => {
 
   // save room
-  test('save room and check existence', async (done) => {
+  test('save room and check existence', async () => {
     roomStore.saveRoom('123', 'robie');
 
     const res = await roomStore.findRoom('123');
     expect(res).toBe(1);
-    done();
   });
 
   // TODO: combine?
-  test('get room owner', async (done) => {
+  test('get room owner', async () => {
     const res = await roomStore.getRoomOwner('123');
     expect(res).toEqual(['robie']);
-    done();
   });
 
   // update room
-  test('update room', async (done) => {
+  test('update room', async () => {
     roomStore.saveRoom('123', 'new robie');
 
     const res = await roomStore.getRoomOwner('123');
     expect(res).toEqual(['new robie']);
-    done();
   });
 
   // remove room
-  test('remove room', async (done) => {
+  test('remove room', async () => {
     roomStore.removeRoom('123');
 
     const res = await roomStore.findRoom('123');
     expect(res).toBe(0);
-    done();
   });
 
   // non existing room
-  test('non existing room', async (done) => {
+  test('non existing room', async () => {
     const res = await roomStore.findRoom('0');
     expect(res).toBe(0);
-    done();
   });
 
 });
@@ -106,39 +97,35 @@ describe('room store', () => {
 describe('room user store', () => {
 
   // save room user
-  test('save room user', async (done) => {
+  test('save room user', async () => {
     roomUserStore.saveRoomUser('123', 'robie');
 
     const res = await roomUserStore.isRoomUser('123', 'robie');
     expect(res).toBe(1);
-    done();
   });
 
   // get all room users
-  test('get all room users', async (done) => {
+  test('get all room users', async () => {
     roomUserStore.saveRoomUser('123', 'another robie');
 
     const res = await roomUserStore.getAllRoomUsers('123');
     expect(res).toEqual(['robie', 'another robie']);
-    done();
   })
 
   // remove room user
-  test('remove room user', async (done) => {
+  test('remove room user', async () => {
     roomUserStore.removeRoomUser('123', 'robie');
 
     const res = await roomUserStore.getAllRoomUsers('123');
     expect(res).toEqual(['another robie']);
-    done();
   });
 
   // remove room
-  test('remove room', async (done) => {
+  test('remove room', async () => {
     roomUserStore.removeRoom('123');
 
     const res = await roomUserStore.roomSize('123');
     expect(res).toBe(0);
-    done();
   });
 
 });
